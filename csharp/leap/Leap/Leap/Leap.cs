d0d126016d92e7e8cb9fe14aeb2a2df37447fe87
asithembiso <asithembiso@gmail.com>
2017-02-16 15:39:59 +0200

    c# leap year solution

---
+++ csharp/leap/Leap/Leap/Leap.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap
+{
+    public class Leap
+    {
+        public static bool IsLeap(int year)
+        {
+            if (year % 400 == 0)
+            {
+                return true;
+            }
+            else if (year % 100 == 0)
+            {
+                return false;
+            }
+            else if (year % 4 == 0)
+            {
+                return true; ;
+            }
+            else
+                return false;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ csharp/leap/Leap/LeapTests/LeapTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Leap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leap.Tests
+{
+    [TestClass()]
+    public class LeapTests
+    {
+
+        [TestMethod()]
+        public void Valid_leap_year()
+        {
+            Assert.IsTrue(Leap.IsLeap(1996));
+        }
+
+        [TestMethod()]
+        public void Invalid_leap_year()
+        {
+            Assert.IsFalse(Leap.IsLeap(1997));
+        }
+        [TestMethod()]
+        public void Turn_of_the_20th_century_is_not_a_leap_year()
+        {
+            Assert.IsFalse(Leap.IsLeap(1900));
+        }
+
+        [TestMethod()]
+        public void Turn_of_the_25th_century_is_a_leap_year()
+        {
+            Assert.IsTrue(Leap.IsLeap(2400));
+        }
+    }
+}
