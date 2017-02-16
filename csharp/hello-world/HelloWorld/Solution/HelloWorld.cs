18aa35c5bf7919b6b025880166b9ed33a73f0647
asithembiso <asithembiso@gmail.com>
2017-02-16 14:49:32 +0200

    Hello World Solution Done

---
+++ csharp/hello-world/HelloWorld/Solution/HelloWorld.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution
+{
+    public class HelloWorld
+    {
+        public HelloWorld() { }
+
+        public static String Hello(string name)
+        {
+            if (name == null) {
+                return "Hello, World!";
+            } else {
+                return "Hello, " + name+"!";
+            }
+        }
+
+        static void Main(string[] args) // static with a void (or int) return type
+        {
+        }
+    }
+}
---
+++ csharp/hello-world/HelloWorld/SolutionTests/HelloWorldTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.Tests
+{
+    [TestClass()]
+    public class HelloWorldTests
+    {
+        [TestMethod()]
+        public void No_name()
+        {
+            Assert.AreEqual(HelloWorld.Hello(null), "Hello, World!");
+        }
+
+        [TestMethod()]
+        public void Sample_name()
+        {
+            Assert.AreEqual(HelloWorld.Hello("Alice"),"Hello, Alice!");
+        }
+
+        [TestMethod()]
+        public void Other_sample_name()
+        {
+            Assert.AreEqual(HelloWorld.Hello("Bob"), "Hello, Bob!");
+        }
+    }
+}
